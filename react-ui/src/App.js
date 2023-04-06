7a09f812898b89da2e3e76272ca6f105c1b261da
Ben Coombes <benzcoombes@gmail.com>
2023-04-06 15:56:18 -0400

    making it look good

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Spinner from 'react-bootstrap/Spinner';
+import { Spinner, Container, Row, Col, Card, Table } from 'react-bootstrap';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 //
 function App() {
   const [data, setData] = useState({});
@@ -12,69 +13,105 @@ function App() {
     const fetchData = async () => {
       axios.get(apiUrl)
         .then(result => {
-          console.log('result.data:',result.data)
+          console.log('result.data:', result.data)
           setData(result.data)
           setShowLoading(false)
         }).catch((error) => {
           console.log('error in fetchData:', error)
         });
-      };  
+    };
     fetchData();
   }, []);
 
   return (
     <div>
-      { showLoading === false
+      {showLoading === false
         ? <div>
-            {showLoading && <Spinner animation="border" role="status">
-              <span className="sr-only">Loading...</span>
-            </Spinner> }
-              
-            <h1>Prediction Results</h1>
-            <h2> the values for species will be:</h2>
-            <li>setosa: 1,0,0</li> 
-            <li>virginica: 0,1,0</li>
-            <li>versicolor: 0,0,1 </li>
+          {showLoading && <Spinner animation="border" role="status">
+            <span className="sr-only">Loading...</span>
+          </Spinner>}
 
-            <table className="App-table">
-              <thead>
-                <tr>
-                  <th>Test 1</th>
-                  <th>Test 2</th>
-                  <th>Test 3</th>
-                </tr>
-              </thead>
-              
-              <tbody>
-                
-                <tr>
-                  <td className="App-td">
-                    { data.row1.map((value, index) => (
-                      <p key={index}>{value}</p>
-                    ))}
-                  </td>
-                  <td className="App-td">
-                  { data.row2.map((value, index) => (
-                    <p key={index}>{value}</p>
-                  ))}
-                  </td>
-                  <td className="App-td">
-                  { data.row3.map((value, index) => (
-                    <p key={index}>{value}</p>
-                  ))}
-                  </td>
+          <Container>
+            <Card border='secondary' bg='dark' text='light' style={{ marginTop: '20px' }}>
+              <Row className='justify-content-md-center mb-2'>
+                <Col sm={12}>
+                  <Card.Title as='h1' className='text-center' style={{ paddingTop: '15px' }}>Prediction Results</Card.Title>
+                </Col>
+              </Row>
+              <Card.Body>
+                <h3 className='text-center'> Flower Species Values:</h3>
+                <Table className='mt-3 text-center' striped bordered hover variant="dark">
+                  <thead>
+                    <tr>
+                      <th width="33%">Setosa</th>
+                      <th width="33%">Virginica</th>
+                      <th width="33%">Versicolor</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>0</td>
+                      <td>0</td>
+                    </tr>
+                    <tr>
+                      <td>0</td>
+                      <td>1</td>
+                      <td>0</td>
+                    </tr>
+                    <tr>
+                      <td>0</td>
+                      <td>0</td>
+                      <td>1</td>
+                    </tr>
+                  </tbody>
+                </Table>
+                <Table className='mt-3' striped bordered hover variant="dark">
+                  <thead>
+                    <tr>
+                      <th>Test 1</th>
+                      <th>Test 2</th>
+                      <th>Test 3</th>
+                    </tr>
+                  </thead>
 
-                </tr>
-              </tbody>
-            </table>
+                  <tbody>
 
-              
-          </div>
-        : 
+                    <tr>
+                      <td className="App-td">
+                        {data.row1.map((value, index) => (
+                          <p key={index}>{value}</p>
+                        ))}
+                      </td>
+                      <td className="App-td">
+                        {data.row2.map((value, index) => (
+                          <p key={index}>{value}</p>
+                        ))}
+                      </td>
+                      <td className="App-td">
+                        {data.row3.map((value, index) => (
+                          <p key={index}>{value}</p>
+                        ))}
+                      </td>
+
+                    </tr>
+                  </tbody>
+                </Table>
+              </Card.Body>
+            </Card>
+          </Container>
+
+
+
+
+
+
+        </div>
+        :
         < div>
           {showLoading && <Spinner animation="border" role="status">
             <span className="sr-only">Waiting for results...</span>
-          </Spinner> }
+          </Spinner>}
         </div>
 
       }
